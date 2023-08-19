f8c89775891c3073e0bc97d97378c8f1055380a5
Anthony Montero Román <anthonymoro08@gmail.com>
2023-08-19 03:42:48 -0700

    Creación de la plataforma
    Diseño de la primera versión de la platforma del cronómetro.

---
+++ platform/platform_inst.v
@@ -0,0 +1,7 @@
+	platform u0 (
+		.clk_clk                                   (<connected-to-clk_clk>),                                   //                                clk.clk
+		.reset_reset_n                             (<connected-to-reset_reset_n>),                             //                              reset.reset_n
+		.pio_segundos_0_external_connection_export (<connected-to-pio_segundos_0_external_connection_export>), // pio_segundos_0_external_connection.export
+		.pio_switch_0_external_connection_export   (<connected-to-pio_switch_0_external_connection_export>)    //   pio_switch_0_external_connection.export
+	);
+
